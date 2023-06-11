49919e0dbdf92f8d704d996235265ebb294799a8
nathan <nathan.stevens.@my.jcu.edu.au>
2023-06-11 11:28:18 +1000

    Refactor password stars program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -4,10 +4,20 @@ converting password to stars
 """
 
 
-minimum_password_length = 5
-password = input("Enter Password >>")
-while len(password) < minimum_password_length:
-    print("Please enter a longer password")
+def main():
+    minimum_password_length = 5
+    password = get_password(minimum_password_length)
+    convert_password_to_stars(password)
+
+
+def convert_password_to_stars(password):
+    for i in range(len(password)):
+        print("*", end="")
+
+
+def get_password(minimum_password_length):
     password = input("Enter Password >>")
-for i in range(len(password)):
-    print("*", end="")
+    while len(password) < minimum_password_length:
+        print("Please enter a longer password")
+        password = input("Enter Password >>")
+    return password
