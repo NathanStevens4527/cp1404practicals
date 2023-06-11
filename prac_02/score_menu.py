28b4ef7105b12497aec22497ea38d6b60aab4471
nathan <nathan.stevens.@my.jcu.edu.au>
2023-06-11 20:07:56 +1000

    Add menu loop and copy get valid score function

---
+++ prac_02/score_menu.py
@@ -0,0 +1,47 @@
+"""
+
+"""
+MENU = "(G)et a score between 1-100\n(P)rint result\n(S)how stars\n(Q)uit"
+
+def main():
+    score = get_valid_score()
+    print(MENU)
+    choice = input(">>")
+    while choice != "Q":
+        if choice == "G":
+            score = get_valid_score()
+        elif choice == "P":
+            score_status = determine_score_status(score)
+            print(f"That score is {score_status}")
+        elif choice == "S":
+            generate_stars(score)
+            print(s)
+        choice = input(">>")
+    print("Good bye")
+
+
+def get_valid_score():
+    score = int(input("Input score >> "))
+    while score > 100 or score < 0:
+        print("Invalid score")
+        score = int(input("Input score >> "))
+    return score
+
+
+def determine_score_status(score):
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+def generate_stars(score):
+    stars = ""
+    for i in range(score):
+        stars += "*"
+    return stars
+
+
+main()
