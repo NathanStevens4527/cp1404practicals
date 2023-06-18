fad2a1ae7b8f034d224c30a307b203b98f6445cd
nathan <nathan.stevens.@my.jcu.edu.au>
2023-06-18 16:27:55 +1000

    Add code for reading and counting numbers from file

--- prac_03/files.py
+++ prac_03/files.py
@@ -14,3 +14,21 @@ in_file = open("name.txt", "r")
 name = in_file.readline()
 in_file.close()
 print(f"Your name is {name}")
+
+
+result = 0
+in_file = open("numbers.txt", "r")
+for i in range (0, 2):
+    number = int(in_file.readline())
+    result += number
+print(f"The sum of the first 2 numbers in numbers.txt is {result}")
+in_file.close()
+
+
+total = 0
+in_file = open("numbers.txt", "r")
+for line in in_file:
+    number = int(line)
+    total += number
+print(f"The total sum of numbers in numbers.txt is {total}")
+in_file.close()
