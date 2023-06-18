53c76568dbad343527324a62f6c7016967bac02f
nathan <nathan.stevens.@my.jcu.edu.au>
2023-06-18 16:05:30 +1000

    add code for writing name to file

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,3 +1,16 @@
 """
 Program for testing various functions for reading and writing files.
 """
+
+
+name = input("Enter Name >>")
+out_file = open("name.txt", "w")
+print(name, file=out_file)
+out_file.close()
+
+name = ""
+print(f"Your name is {name}")
+in_file = open("name.txt", "r")
+name = in_file.readline()
+in_file.close()
+print(f"Your name is {name}")
