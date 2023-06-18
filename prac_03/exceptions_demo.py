688f2ac6cb0085fee63d20af10185d8c4923172d
nathan <nathan.stevens.@my.jcu.edu.au>
2023-06-18 13:49:10 +1000

    Add error checking for denominator

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -13,6 +13,9 @@ An error checking while loop could be implemented to keep asking for an integer
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
+    while denominator == 0:
+        print("Denominator cannot be 0")
+        denominator = int(input("Enter the denominator: "))
     fraction = numerator / denominator
     print(fraction)
 except ValueError:
