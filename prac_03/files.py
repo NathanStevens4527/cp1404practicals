6f6efa114dd7b01c2f73995bfaf48fe6e64df438
nathan <nathan.stevens.@my.jcu.edu.au>
2023-06-18 16:09:10 +1000

    Add code for reading name from file

--- prac_03/files.py
+++ prac_03/files.py
@@ -8,7 +8,7 @@ out_file = open("name.txt", "w")
 print(name, file=out_file)
 out_file.close()
 
-name = ""
+
 print(f"Your name is {name}")
 in_file = open("name.txt", "r")
 name = in_file.readline()
