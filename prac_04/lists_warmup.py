6c1a7a23dbeac0e79267c4f9b396e66b80d7a9bf
nathan <nathan.stevens.@my.jcu.edu.au>
2023-06-24 13:39:14 +1000

    add answers for list comprehension statements

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,25 +1,32 @@
 """
-
+Program for answering list comprehension warmup questions
+and writing list comprehension statements.
 """
 
 
 numbers = [3, 1, 4, 1, 5, 9, 2]
 
-numbers[0]
+# numbers[0]
 # 3
-numbers[-1]
+# numbers[-1]
 # 2
-numbers[3]
+# numbers[3]
 # 1
-numbers[:-1]
+# numbers[:-1]
 # [3, 1, 4, 1, 5, 9]
-numbers[3:4]
+# numbers[3:4]
 # [1]
-5 in numbers
+# 5 in numbers
 # True
-7 in numbers
+# 7 in numbers
 # False
-"3" in numbers
+# "3" in numbers
 # False
-numbers + [6, 5, 3]
+# numbers + [6, 5, 3]
 # [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+numbers[0] = "ten"
+numbers[-1] = 1
+print(numbers[3:])
+print(9 in numbers)
+print(numbers)
