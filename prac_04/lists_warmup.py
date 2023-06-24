c66d2f6b1495231fe4e3ab8b90623d630681858d
nathan <nathan.stevens.@my.jcu.edu.au>
2023-06-24 13:15:40 +1000

    add answers for list warm ups

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -12,14 +12,14 @@ numbers[-1]
 numbers[3]
 # 1
 numbers[:-1]
-# 3141592
+# [3, 1, 4, 1, 5, 9]
 numbers[3:4]
-# 15
+# [1]
 5 in numbers
 # True
 7 in numbers
 # False
 "3" in numbers
-# True
+# False
 numbers + [6, 5, 3]
 # [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
