c34997f0231735b035fe5513e90405577bba226c
nathan <nathan.stevens.@my.jcu.edu.au>
2023-06-24 13:08:19 +1000

    add file for list warmups

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,25 @@
+"""
+
+"""
+
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+numbers[0]
+# 3
+numbers[-1]
+# 2
+numbers[3]
+# 1
+numbers[:-1]
+# 3141592
+numbers[3:4]
+# 15
+5 in numbers
+# True
+7 in numbers
+# False
+"3" in numbers
+# True
+numbers + [6, 5, 3]
+# [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
