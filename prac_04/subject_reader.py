fa2ac3414295b82d0c528fad93eda78dc090bb7e
nathan <nathan.stevens.@my.jcu.edu.au>
2023-06-25 20:33:00 +1000

    change get data function to return data as list of lists

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -9,11 +9,16 @@ FILENAME = "subject_data.txt"
 def main():
     data = get_data()
     print(data)
+    for subject in data:
+        subject_key = 0
+        print(f"{subject[0]} is taught by {subject[1]} and has {subject[2]} students")
+        subject_key += 1
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    subjects = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -23,7 +28,9 @@ def get_data():
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+        subjects += [parts]
     input_file.close()
+    return subjects
 
 
 main()
