1b15f73b3f9ec9e28c16305710bc071f51d67db9
nathan <nathan.stevens.@my.jcu.edu.au>
2023-07-02 00:41:40 +1000

    Add docstring for emails

---
+++ prac_05/emails.py
@@ -0,0 +1,7 @@
+"""
+Program for getting emails and deriving names
+if a name can't be derived from and email the program will ask for a name
+the emails and names will be stored in a dictionairy.
+"""
+
+emails_to_names = {}
