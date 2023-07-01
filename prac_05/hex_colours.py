4c338d815490898c4c1b8aaf752791c7c8dcfa21
nathan <nathan.stevens.@my.jcu.edu.au>
2023-07-01 23:44:25 +1000

    Add docstring

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,3 +1,7 @@
+"""
+program for retrieving hex codes for colours stored in a dictionary
+"""
+
 HEX_CODE_TO_COLOUR = {"RED": "#ff0000", "BLUE": "#0000ff", "GREEN": "#00ff00", "YELLOW": "#ffff00",
                       "ORANGE": "#ffa500", "PURPLE": "#a020f0", "BLACK": "#000000", "WHITE": "#ffffff",
                       "BROWN": "#a52a2a", "GRAY": "#bebebe"}
