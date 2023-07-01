4a288ad4b12cd346bfd8923761741a7e052db437
nathan <nathan.stevens.@my.jcu.edu.au>
2023-07-01 23:18:40 +1000

    Add constant dictionary for hex_colours

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,4 @@
+HEX_CODE_TO_COLOUR = {"RED": "#ff0000", "BLUE": "#0000ff", "GREEN": "#00ff00", "YELLOW": "#ffff00",
+                      "ORANGE": "#ffa500", "PURPLE": "#a020f0", "BLACK": "#000000", "WHITE": "#ffffff",
+                      "BROWN": "#a52a2a", "GRAY": "#bebebe"}
+
