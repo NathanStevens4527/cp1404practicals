503d16951fa66f6e9cf2f2444b20fd8ceb7e1d28
nathan <nathan.stevens.@my.jcu.edu.au>
2023-07-01 23:31:02 +1000

    add loop for getting colour

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -2,3 +2,10 @@ HEX_CODE_TO_COLOUR = {"RED": "#ff0000", "BLUE": "#0000ff", "GREEN": "#00ff00", "
                       "ORANGE": "#ffa500", "PURPLE": "#a020f0", "BLACK": "#000000", "WHITE": "#ffffff",
                       "BROWN": "#a52a2a", "GRAY": "#bebebe"}
 
+colour = input("Enter colour: ").upper()
+while colour != "":
+    try:
+        print(f"The Hex code for {colour.title()} is {HEX_CODE_TO_COLOUR[colour]}")
+    except KeyError:
+        print("Invalid colour")
+    colour = input("Enter colour: ").upper()
