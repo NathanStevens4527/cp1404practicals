7d44b5f9b1eb98b9a88867aa0a5c655e17a1e15a
nathan <nathan.stevens.@my.jcu.edu.au>
2023-07-02 00:29:32 +1000

    Add loop for counting word occurrences in string and sorting

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -5,6 +5,13 @@ words are stored in dictionary with the number of occurrences.
 
 word_to_occurrences = {}
 text = input("Text: ")
+text = text.split()
 
 for word in text:
-    print(word)
+    if word in word_to_occurrences:
+        word_to_occurrences[word] += 1
+    else:
+        word_to_occurrences[word] = 1
+sorted_word_to_occurrences = sorted(word_to_occurrences)
+for word in sorted_word_to_occurrences:
+    print(f"{word} : {word_to_occurrences[word]}")
