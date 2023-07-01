c46ccf38953684c4df1aff7e55b7678a9d94dfba
nathan <nathan.stevens.@my.jcu.edu.au>
2023-07-01 23:54:06 +1000

    Add docstring for word_occurrences

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,10 @@
+"""
+Program for counting the number of occurrences of a word in a string
+words are stored in dictionary with the number of occurrences.
+"""
+
+word_to_occurrences = {}
+text = input("Text: ")
+
+for word in text:
+    print(word)
