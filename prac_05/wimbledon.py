b39ad5941f0cc21d7ef48822fe60949e7ab6bbe3
nathan <nathan.stevens.@my.jcu.edu.au>
2023-07-02 13:51:52 +1000

    Add function for reading data from file

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,10 +1,28 @@
 """
 program reads file and processes data for champions and countries
-displays how many times each champion and country has won.
+displays how many times each champion has won and their country.
 """
+
+FILENAME = "wimbledon.csv"
+COUNTRY_INDEX = 1
+CHAMPION_INDEX = 2
 
 
 def main():
     champion_to_win = {}
-    country_to_win = {}
+    countries = {}
+    data = get_data(FILENAME)
+    print("DONE")
 
+
+def get_data(file):
+    data = []
+    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        for line in in_file:
+            line = line.split(",")
+            data.append(line)
+        return data
+
+
+main()
