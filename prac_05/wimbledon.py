a2880f8fc95eac5f70783a4200aca6d5e0e17d89
nathan <nathan.stevens.@my.jcu.edu.au>
2023-07-02 12:18:07 +1000

    Add docstring and main function for wimbledon

---
+++ prac_05/wimbledon.py
@@ -0,0 +1,10 @@
+"""
+program reads file and processes data for champions and countries
+displays how many times each champion and country has won.
+"""
+
+
+def main():
+    champion_to_win = {}
+    country_to_win = {}
+
