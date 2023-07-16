2dc5952ece586838320e0509bc2aeac793edeae3
nathan <nathan.stevens.@my.jcu.edu.au>
2023-07-16 21:08:13 +1000

    add file for guitar module

---
+++ prac_06/guitar.py
@@ -0,0 +1,8 @@
+"""
+Module for storing attributes for guitars
+Estimated completion time 50 minutes
+"""
+class Guitar:
+    """Represents a guitar object"""
+
+    def __init__(self, name, year, cost):
