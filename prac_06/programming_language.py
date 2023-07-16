30de661bb768bf39f23c0e893f897738ee554162
nathan <nathan.stevens.@my.jcu.edu.au>
2023-07-16 20:27:59 +1000

    add __init__ __str__ and method for checking typing status

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -2,3 +2,21 @@
 module for storing programming languages.
 Estimated time to complete: 1 hour
 """
+
+
+class ProgrammingLanguage:
+
+    def __init__(self, name, typing, reflection, year):
+        """store the name, typing, reflection and year of programming language"""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        """Generates string for programming language"""
+        return f"{self.name},{self.typing} Typing, Reflection = {self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        """Determines if passed in langiage is dynamic"""
+        return self.typing == "Dynamic"
