561a3ff64d0f4ed38ac3bda33c56f6e455722a5d
nathan <nathan.stevens.@my.jcu.edu.au>
2023-07-16 20:48:10 +1000

    Add print statement for langauges

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -10,6 +10,7 @@ visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 print(python)
 
 programming_languages = [python, ruby, visual_basic]
+print("The dynamic languages are:")
 for programming_language in programming_languages:
     if programming_language.is_dynamic():
         print(programming_language)
