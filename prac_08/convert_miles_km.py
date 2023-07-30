0c342735f62abd57389becbdc5dd5a8600a6e318
nathan <nathan.stevens.@my.jcu.edu.au>
2023-07-30 21:14:15 +1000

    add function for converting miles to kilometers

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -13,5 +13,13 @@ class ConvertMilesToKilometersApp(App):
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
+    def handle_convert_to_kilometers(self, miles):
+        try:
+            kilometers = float(miles) * 1.609
+            self.root.ids.output_result.text = str(kilometers)
+        except TypeError:
+            self.root.ids.input_mile.text = '0'
+
+
 
 ConvertMilesToKilometersApp().run()
