322c1a0563158bd4e744390f04b4116db921f9f6
nathan <nathan.stevens.@my.jcu.edu.au>
2023-08-06 22:11:58 +1000

    add methods for silver and tests

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -3,6 +3,6 @@ programs for testing silver service taxi
 """
 from prac_09.silver_service_taxi import SilverServiceTaxi
 
-my_silver_taxi = SilverServiceTaxi("Fancy taxi", 1000, 20)
-my_silver_taxi.drive(30)
+my_silver_taxi = SilverServiceTaxi("Fancy taxi", 1000, 2)
+my_silver_taxi.drive(18)
 print(f"{my_silver_taxi} fare = {my_silver_taxi.calculate_silver_service_fare()}")
