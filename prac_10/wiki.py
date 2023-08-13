d5e522335f860e4ec7ab1d9978375a113c024eb3
nathan <nathan.stevens.@my.jcu.edu.au>
2023-08-13 17:58:20 +1000

    Complete pracwork for wiki task

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -2,5 +2,6 @@ import wikipedia
 
 search = input("Search >>> ")
 while search != "":
-    print(wikipedia.summary(search))
+    page = wikipedia.page(search)
+    print(page.title, page.summary, page.url)
     search = input("Search >>> ")
